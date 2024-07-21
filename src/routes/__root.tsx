5a5602ba0caedc436c3236f08e4e061629bf5b8e
Thomas McInnis <hey@thomasmcinnis.com>
2024-07-21 17:46:29 +1000

    Remove Tanstack Devtools and fix TS error

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import Credits from '../components/Credits';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
       <Credits />
     </>
   ),
