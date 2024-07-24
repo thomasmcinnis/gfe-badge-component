19e7300b20ddf782b86fec7f97d235897f5978e6
Thomas McInnis <hey@thomasmcinnis.com>
2024-07-24 20:37:41 +1000

    Add index with links to competed projects

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -4,10 +4,34 @@ export const Route = createLazyFileRoute('/')({
   component: Index,
 });
 
+const projects = ['badge-component', 'badge-component'];
+
+function prettyName(project: string) {
+  return project
+    .split('-')
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+}
+
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="m-8 text-zinc-800">
+      <h1 className="text-2xl pb-2">Completed React Projects</h1>
+      <p className="text-sm text">
+        The following are simple components built for challenges on
+        GreatFrontEnd Projects.
+      </p>
+      <ul className="pt-6">
+        {projects.map((project) => (
+          <li>
+            <a href={`/${project}`} className="underline">
+              {prettyName(project)}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
